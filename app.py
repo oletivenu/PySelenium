470c7440c69695262002efbf006ec74235f76919
Naga Venu Babu  Oleti <N.Oleti@midisglobal.local>
2020-01-14 18:25:11 +0400

    element instead of elements

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ browser.get("https://github.com")
 signin_link = browser.find_element_by_link_text("Sign in")
 signin_link.click()
 
-username_box = browser.find_elements_by_id("login_field")
-username_box[0].send_keys("venu.babu80@gmail.com")
+username_box = browser.find_element_by_id("login_field")
+username_box.send_keys("venu.babu80@gmail.com")
 
-password_box = browser.find_elements_by_id("password")
-password_box[0].send_keys("Venubabu%12345")
+password_box = browser.find_element_by_id("password")
+password_box.send_keys("Venubabu%12345")
 
-password_box[0].submit()
+password_box.submit()
 
 assert "oletivenu" in browser.page_source
 
-signout_link = browser.find_elements_by_class_name("dropdown-signout")
+signout_link = browser.find_element_by_class_name("dropdown-signout")
 
-signout_link[0].submit()
+signout_link.submit()
 
 browser.quit()
